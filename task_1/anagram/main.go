ff099805d27e627fd3c4be19ed0b17128b80691e
Dubrova Dzmitry <70838468+dimaxdqwerty@users.noreply.github.com>
2021-03-21 10:03:20 +0300

    add implementation of anagram in task_1

--- task_1/anagram/main.go
+++ task_1/anagram/main.go
@@ -1,20 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
 /*
   Description: An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia https://en.wikipedia.org/wiki/Anagram).
   Note: anagrams are case insensitive
-
   Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
-
   Examples:
   "foefet" is an anagram of "toffee"
   "Buckethead" is an anagram of "DeathCubeK"
 */
 
 func isAnagram(test, original string) bool {
-	panic("Implement me!")
+	test, original = strings.ToLower(test), strings.ToLower(test)
+	arrTest, arrOriginal := strings.Split(test, ""), strings.Split(original, "")
+	return reflect.DeepEqual(arrTest, arrOriginal)
 }
 
 func main() {
-
+	test := "Buckethead"
+	original := "DeathCubeK"
+	fmt.Println(isAnagram(test, original))
 }
