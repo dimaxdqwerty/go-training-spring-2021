7563f07851ee1ca05a8f78a35d599595fa157b84
Dubrova Dzmitry <70838468+dimaxdqwerty@users.noreply.github.com>
2021-03-21 11:54:54 +0300

    add implementation of even_odd in task_1

--- task_1/even_odd/main.go
+++ task_1/even_odd/main.go
@@ -1,14 +1,22 @@
 package main
 
+import "fmt"
+
 /*
  Description:
  Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
 
 func evenOrOdd(number int) string {
-	panic("Implement me!")
+	var answer string
+	if number%2 == 0 {
+		answer = "Even"
+	} else {
+		answer = "Odd"
+	}
+	return answer
 }
 
 func main() {
-
+	fmt.Println(evenOrOdd(6))
 }
