9f0e88f7f102455dc155e796fecc7c7d8a6d9cac
Dubrova Dzmitry <70838468+dimaxdqwerty@users.noreply.github.com>
2021-03-21 11:59:25 +0300

    add implementation of factorial in task_1

--- task_1/factorial/main.go
+++ task_1/factorial/main.go
@@ -1,18 +1,22 @@
 package main
 
+import "fmt"
+
 /*
 	In mathematics, the factorial of a non-negative integer n, denoted by n!,
  	is the product of all positive integers less than or equal to n.
-
  	For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
-
 	Write a function to calculate factorial for a given input.
 */
 
 func factorial(n int) int {
-	panic("Implement me!")
+	factorial := 1
+	for i := n; i > 0; i-- {
+		factorial *= i
+	}
+	return factorial
 }
 
 func main() {
-
+	fmt.Println(factorial(6))
 }
