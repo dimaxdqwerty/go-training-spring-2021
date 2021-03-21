e2d8714819eb2acbca05a098aedc24c258c57eef
Dubrova Dzmitry <70838468+dimaxdqwerty@users.noreply.github.com>
2021-03-21 10:34:01 +0300

    add implementation of century in task_1

--- task_1/centry/main.go
+++ task_1/centry/main.go
@@ -1,11 +1,11 @@
 package main
 
+import "fmt"
+
 /*
 	Description: The first century spans from the year 1 up to and including the year 100,
 	The second - from the year 101 up to and including the year 200, etc.
-
 	Given a year, return the century it is in.
-
 	Examples:"
 	centuryFromYear(1705)  returns (18)
 	centuryFromYear(1900)  returns (19)
@@ -14,9 +14,10 @@ package main
 */
 
 func century(year int) int {
-	panic("Implement me!")
+	var century = ((year-1)/100)+1
+	return century
 }
 
 func main() {
-
+	fmt.Println(century(2000))
 }
