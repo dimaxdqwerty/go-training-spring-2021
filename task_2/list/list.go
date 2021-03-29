b3bbf37eb48e9afc313f8958d25d70e16c29e5ee
Dubrova Dzmitry <70838468+dimaxdqwerty@users.noreply.github.com>
2021-03-29 11:05:17 +0300

    add sort method to list and queue

--- task_2/list/list.go
+++ task_2/list/list.go
@@ -1,7 +1,8 @@
-package list
+package main23
 
 import (
 	"fmt"
+	"sort"
 )
 
 type Node struct {
@@ -12,6 +13,29 @@ type Node struct {
 type LinkedList struct {
 	head *Node
 	len int
+}
+
+func (L *LinkedList) Len() int {
+	return L.len
+}
+
+func (L *LinkedList) Less(i, j int) bool {
+	i1 := L.GetAtPos(i).item
+	i2 := L.GetAtPos(j).item
+	switch i1.(type) {
+	case int:
+		return i1.(int) < i2.(int)
+	case float64:
+		return i1.(float64) < i2.(float64)
+	case string:
+		return i1.(string) < i2.(string)
+	default:
+		panic("Unknown type")
+	}
+}
+
+func (L *LinkedList) Swap(i, j int) {
+	L.GetAtPos(i).item, L.GetAtPos(j).item = L.GetAtPos(j).item, L.GetAtPos(i).item
 }
 
 func (L *LinkedList) Insert(item interface{}) {
@@ -77,17 +101,23 @@ func (L *LinkedList) GetAtPos(id int) *Node {
 	return ptr
 }
 
+func (L *LinkedList) Sort() {
+	sort.Sort(L)
+}
+
 func main() {
 	list := LinkedList{}
 	list.Insert(1)
-	list.Insert(3)
+	list.Insert(4)
 	list.Insert(2)
 	list.Insert(5)
-	list.Insert(4)
+	list.Insert(3)
 	list.Display()
 	fmt.Println(list.Search(3))
 	list.Deletion()
 	list.Display()
 	list.Delete(2)
+	list.Display()
+	list.Sort()
 	list.Display()
 }
--- task_2/queue/queue.go
+++ task_2/queue/queue.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 type Node struct {
@@ -15,9 +16,27 @@ type Queue struct {
 	len int
 }
 
-func NewQueue() *Queue {
-	q := &Queue{}
-	return q
+func (Q *Queue) Len() int {
+	return Q.len
+}
+
+func (Q *Queue) Less(i, j int) bool {
+	i1 := Q.GetAtPos(i).item
+	i2 := Q.GetAtPos(j).item
+	switch i1.(type) {
+	case int:
+		return i1.(int) < i2.(int)
+	case float64:
+		return i1.(float64) < i2.(float64)
+	case string:
+		return i1.(string) < i2.(string)
+	default:
+		panic("Unknown type")
+	}
+}
+
+func (Q *Queue) Swap(i, j int) {
+	Q.GetAtPos(i).item, Q.GetAtPos(j).item = Q.GetAtPos(j).item, Q.GetAtPos(i).item
 }
 
 func (Q *Queue) Enqueue(item interface{}) {
@@ -45,14 +64,27 @@ func (Q *Queue) Peek() interface{} {
 	return n.item
 }
 
+func (Q *Queue) GetAtPos(id int) *Node {
+	ptr := Q.head
+	for i := 0; i < id; i++ {
+		ptr = ptr.next
+	}
+	return ptr
+}
+
+func (Q *Queue) Sort() {
+	sort.Sort(Q)
+}
+
 func main() {
-	queue := NewQueue()
+	queue := Queue{}
 	queue.Enqueue(1)
-	queue.Enqueue(2)
-	queue.Enqueue(3)
-	queue.Enqueue(4)
 	queue.Enqueue(5)
+	queue.Enqueue(3)
+	queue.Enqueue(2)
+	queue.Enqueue(4)
 	fmt.Println(queue.Peek())
 	queue.Dequeue()
 	fmt.Println(queue.Peek())
+	queue.Sort()
 }
